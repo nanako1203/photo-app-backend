a10e6199e68eeba14e0d80a9f9a974c75987ca5e
nanako1203 <ououtlierhuan@gmail.com>
2025-08-21 15:59:36 +0900

    controller are completed

--- src/main/java/com/caihuan/photo_app_backend/controller/PhotoController.java
+++ src/main/java/com/caihuan/photo_app_backend/controller/PhotoController.java
@@ -1,7 +1,10 @@
 package com.caihuan.photo_app_backend.controller;
 
 import com.caihuan.photo_app_backend.entity.Photo;
+import com.caihuan.photo_app_backend.payload.dto.ImageAnalysisResult;
+import com.caihuan.photo_app_backend.payload.request.AiAnalysisRequest;
 import com.caihuan.photo_app_backend.repository.PhotoRepository;
+import com.caihuan.photo_app_backend.services.ImageAnalysisService;
 import com.caihuan.photo_app_backend.services.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @Author nanako
@@ -27,6 +32,8 @@ public class PhotoController {
     private S3Service s3Service;
     @Autowired
     private PhotoRepository photoRepository;
+    @Autowired
+    private ImageAnalysisService imageAnalysisService;
 
     //获取指定相册所有照片
     @GetMapping("/album/{albumId}")//{albumId}路径变量 用实际id替换
@@ -48,11 +55,23 @@ public class PhotoController {
         try {
             //上传到aws并返回公开访问地址
             String fileUrl = s3Service.upLoadFile(file);
+            ImageAnalysisResult analysisResult = imageAnalysisService.analyzeImage(file);
 
             //将url获取的内容设置到新的photo对象
             Photo photo = new Photo();
             photo.setAlbumId(albumId);
             photo.setStorageUrl(fileUrl);
+            photo.setSceneCategory(analysisResult.getSceneCategory());
+            if (analysisResult.getLabels() != null) {
+                photo.setAiLabels(String.join(",", analysisResult.getLabels()));
+            }
+            if (analysisResult.getDetectedText() != null) {
+                photo.setAiDetectedText(analysisResult.getDetectedText());
+            }
+            if (analysisResult.getDominantColors() != null) {
+                photo.setAiDominantColors(String.join(",", analysisResult.getDominantColors()));
+            }
+
             photoRepository.save(photo);
 
             //保存到数据库的photo表中
@@ -61,6 +80,24 @@ public class PhotoController {
 
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("文件上传失败" + e.getMessage());
+        }
+    }
+
+    //web端请求云端ai精选
+    @PostMapping("/analyze-selection")
+    public ResponseEntity<?> analyzeSelectedPhotos(@RequestBody AiAnalysisRequest request) {
+        List<ImageAnalysisResult> results = new ArrayList<>();
+        for (Long photoId : request.getPhotoIds()) {
+            Optional<Photo> photoOpt = photoRepository.findById(photoId);
+            if (photoOpt.isPresent()) {
+                try {
+                    byte[] imageBytes = s3Service.downloadFile(photoOpt.get().getStorageUrl());
+                    ImageAnalysisResult result = imageAnalysisService.analyzeImageBytes(imageBytes);
+                    results.add(result);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
